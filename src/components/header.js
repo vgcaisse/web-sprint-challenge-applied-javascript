897438b865e800b9aa0e79c43c7a0756583d3a39
Visnu <vgcaisse@gmail.com>
2021-12-03 16:43:25 -0500

    still working...

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Header = (title, date, temp) => {
   // TASK 1
   // ---------------------
@@ -13,9 +15,13 @@ const Header = (title, date, temp) => {
   //
 
   const header = document.createElement('div');
-  const date = document.createElement('span');
-  const title = document.createElement('h1');
-  const temp = document.createElement('span');
+  const dated = document.createElement('span');
+  const titled = document.createElement('h1');
+  const temped = document.createElement('span');
+
+  // date.textContent = obj.date;
+  // title.textContent = obj.title;
+  // temp.textContent = obj.temp;
 
   header.appendChild(date);  
   header.appendChild(title);
@@ -28,14 +34,20 @@ const Header = (title, date, temp) => {
   return header;
 }
 
-const headerAppender = (selector) => {
+const headerAppender = (header) => {
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-  document.querySelector('.header').appendChild(Header(title, date, temp));
+  axios.get(header)
+  .then(resp => {
+    // document.querySelector('.header').appendChild(Header(resp.data));
+    console.log(resp);
+  })
+  .catch(err=> console.error(err));
+  
 }
 
 export { Header, headerAppender }
